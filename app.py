2f4d78921fb2da663f9e8b97b50d6cea8e291492
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2021-05-05 22:06:34 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import numpy as np
+import requests
+import streamlit as st
+
+ENDPOINT = "https://cataas.com/cat"
+N_COLS = 4
+
+with st.sidebar:
+    st.header("Configuration")
+    n_photos = st.slider("Number of cat photos:", 4, 32, 16)
+    with st.beta_expander("About this app"):
+        st.markdown("It's about cats :cat:!")
+    st.caption("Source: https://cataas.com/#/")
+
+st.title("Choose your favorite cat :cat:")
+
+cat_images = [requests.get("https://cataas.com/cat?width=400&height=400").content for i in range(n_photos)]
+n_rows = 1 + len(cat_images) // N_COLS
+rows = [st.beta_container() for _ in range(n_rows)]
+cols_per_row = [r.beta_columns(N_COLS) for r in rows]
+
+for image_index, cat_image in enumerate(cat_images):
+    with rows[image_index // N_COLS]:
+        cols_per_row[image_index // N_COLS][image_index % N_COLS].image(cat_image)
