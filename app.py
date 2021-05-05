6b8153e2009944ccd114cdbeab98c1176a669d44
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2021-05-05 22:14:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,25 @@ import requests
 import streamlit as st
 
 ENDPOINT = "https://cataas.com/cat"
-N_COLS = 4
 
 with st.sidebar:
     st.header("Configuration")
-    n_photos = st.slider("Number of cat photos:", 4, 64, 16)
+    with st.form(key="grid_reset"):
+        n_photos = st.slider("Number of cat photos:", 4, 128, 16)
+        n_cols = st.number_input("Number of columns", 2, 8, 4)
+        st.form_submit_button(label="Reset images and layout")
     with st.beta_expander("About this app"):
         st.markdown("It's about cats :cat:!")
     st.caption("Source: https://cataas.com/#/")
 
 st.title("Choose your favorite cat :cat:")
+st.caption("You can display the image in full size by hovering it and clicking the double arrow")
 
 cat_images = [requests.get("https://cataas.com/cat?width=400&height=400").content for i in range(n_photos)]
-n_rows = 1 + len(cat_images) // N_COLS
+n_rows = 1 + len(cat_images) // n_cols
 rows = [st.beta_container() for _ in range(n_rows)]
-cols_per_row = [r.beta_columns(N_COLS) for r in rows]
+cols_per_row = [r.beta_columns(n_cols) for r in rows]
 
 for image_index, cat_image in enumerate(cat_images):
-    with rows[image_index // N_COLS]:
-        cols_per_row[image_index // N_COLS][image_index % N_COLS].image(cat_image)
+    with rows[image_index // n_cols]:
+        cols_per_row[image_index // n_cols][image_index % n_cols].image(cat_image)
